5615f023a51950b33efb1a5dfd7e1553ee66bd41
Joe Reeve <joseph.reeve@googlemail.com>
2015-10-22 09:48:15 +0100

    added file uploads

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var jira = require('jira-api');
 var Mustache = require('mustache');
+var request = require('request');
+var fs =  require('fs');
 
-var summaryTemplate = require('fs').readFileSync(__dirname + '/summaryTemplate.mustache', 'utf8');
-var reportTemplate = require('fs').readFileSync(__dirname + '/reportTemplate.mustache', 'utf8');
+var summaryTemplate = fs.readFileSync(__dirname + '/summaryTemplate.mustache', 'utf8');
+var reportTemplate = fs.readFileSync(__dirname + '/reportTemplate.mustache', 'utf8');
 
 module.exports = function(config){
 
@@ -19,48 +21,67 @@ module.exports = function(config){
   return function(results, opts, done) {
     console.log('Jira-Reporter is go...');
 
-      Object.keys(results.modules).map(function(key){
-        results.modules[key].module = key;
-        return results.modules[key];
-      }).filter(function(item){
-        return item.failures > 0 || item.errors > 0;
-      }).map(function(item){
-        item.failed = Object.keys(item.completed).reduce(function(a, b){
-          var ret = item.completed[b];
-          ret.name = b;
-          return a.concat(ret);
-        }, []).filter(function(item){
-          return item.failed || item.errors;
-        });
-        return item;
-      }).reduce(function(a, b){
-
-        var ret = {
-          module:b.module,
-          summary: Mustache.render(config.summaryTemplate || summaryTemplate, b),
-          description: Mustache.render(config.reportTemplate || reportTemplate, b),
-          labels: b.module.split(/\\|\//g).concat(config.labels || []),
-          issuetype: { name: config.issueType || 'TestFail'},
-          project: { key: config.project || 'TEST' },
-        };
-
+    Object.keys(results.modules).map(function(key){
+      results.modules[key].module = key;
+      return results.modules[key];
+    }).filter(function(item){
+      return item.failures > 0 || item.errors > 0;
+    }).map(function(item){
+      item.failed = Object.keys(item.completed).reduce(function(a, b){
+        var ret = item.completed[b];
+        ret.name = b;
         return a.concat(ret);
-      }, []).forEach(function(issue){
-        var fileDir = config.projectRoot + config.imgPath + issue.module.split(/\\|\//g).join('\\');
-        var filePath = fileDir + '\\' + require('fs').readdirSync(fileDir).
-        sort(function(a, b){
-          return a > b;
-        })[0];
-        delete issue.module;
-        options.data = {
-          fields:issue,
-          file:filePath
-        };
-        jira.issue.post(options, function(err, response) {
-          if (err.errors && err.errors.length)
-            console.error(err.errors);
-        });
+      }, []).filter(function(item){
+        return item.failed || item.errors;
       });
-    done();
-  };
+      return item;
+    }).reduce(function(a, b){
+
+      var ret = {
+        module:b.module,
+        summary: Mustache.render(config.summaryTemplate || summaryTemplate, b),
+        description: Mustache.render(config.reportTemplate || reportTemplate, b),
+        labels: b.module.split(/\\|\//g).concat(config.labels || []),
+        issuetype: { name: config.issueType || 'TestFail'},
+          project: { key: config.project || 'TEST' },
+    };
+
+              return a.concat(ret);
+  }, []).forEach(function(issue){
+    var fileDir = config.projectRoot + config.imgPath + issue.module.split(/\\|\//g).join('\\');
+    var filePath = fileDir + '\\' +fs.readdirSync(fileDir).
+    sort(function(a, b){
+      return a > b;
+    })[0];
+    delete issue.module;
+    options.data = {
+      fields:issue
+    };
+    jira.issue.post(options, function(response) {
+      if(response){
+        if (response && response.errors && response.errors.length)
+          return console.error(response.errors);
+
+        var url = Mustache.render('https://{{host}}/rest/api/2/issue/'+response.key+'/attachments', config);
+        request.post({
+          url:url,
+          headers: {
+            'X-Atlassian-Token':'nocheck'
+          },
+          auth:{
+            user:config.username,
+            pass:config.password
+          },
+          formData: {
+            file: fs.createReadStream(filePath)
+          }
+        }, function(err, res){
+          console.error(err);
+        });
+
+      };
+    });
+  });
+  done();
+};
 };
